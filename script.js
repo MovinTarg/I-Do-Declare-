d0e9e216e88985d32fe750cdcdbf98f75bd5b3d7
Pete <pbianchi192@gmail.com>
2018-01-29 18:51:48 -0600

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+var myTrips = 5;
+var bankMoney = -500;
+var income = 0;
+var moneyPerTrip = 0.0000;
+var name = "MovinTarg";
+var nextTrip = "Seoul";
+var lastTrip = "Italy";
+var leavingSoon = true;
+var happy = true;
+var trips
+
+console.log ("Trips taken: " + myTrips);
+console.log ("Money for new trips: " + bankMoney);
+console.log ("Current income: " + income);
+console.log ("Money per trip: " + moneyPerTrip);
+
+console.log ("Name: " + name);
+console.log ("Next trip: " + nextTrip);
+console.log ("Last trip: " + lastTrip);
+
+console.log ("Leaving ona a new trip soon: " + leavingSoon);
+console.log ("Happy about it: " + happy);
+
+console.log ("Trips planned: " + trips);
